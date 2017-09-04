5017ff84e39984d67c5c9242993c871a1adf6285
Amir Mohtasebi <amir.mohtasebi@gmail.com>
2017-09-04 12:08:17 +1200

    Add data annotation to models

--- refactor-me.Models/Product.cs
+++ refactor-me.Models/Product.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 namespace refactor_me.Models
 {
-    //TODO: Annotate
     public class Product
     {
+        private const string ProductNameRequiredErrorMessage = "Product name is mandatoy";
+        private const string ProductNameTooLongErrorMessage = "Product name cannot be more than 100 characters";
+        private const string ProductDescriptionTooLongErrorMessage = "Product description cannot be more than 500 characters";
+
         public Guid Id { get; set; }
 
+        [Required (AllowEmptyStrings =false, ErrorMessage = ProductNameRequiredErrorMessage)]
+        [MaxLength(100, ErrorMessage = ProductNameTooLongErrorMessage)]
         public string Name { get; set; }
 
+        [MaxLength(500, ErrorMessage = ProductDescriptionTooLongErrorMessage)]
         public string Description { get; set; }
+
 
         public decimal Price { get; set; }
 
--- refactor-me.Models/ProductOption.cs
+++ refactor-me.Models/ProductOption.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace refactor_me.Models
 {
     public class ProductOption
     {
+        private const string ProductIsRequiredErrorMessage = "Product option requires a valid product parent";
+        private const string ProductOptionNameIsRequiredErrorMessage = "Product option name is required";
+        private const string ProductOptionNameTooLongErrorMessage = "Product option name cannot be more than 100 characters";
+        private const string ProductOptionDescriptionTooLongErrorMessage = "Product option description cannot be more than 500 characters";
+
         public Guid Id { get; set; }
 
+        [Required(ErrorMessage = ProductIsRequiredErrorMessage)]
         public Guid ProductId { get; set; }
 
+        [Required(ErrorMessage = ProductOptionNameIsRequiredErrorMessage)]
+        [MaxLength(100, ErrorMessage = ProductOptionNameTooLongErrorMessage)]
         public string Name { get; set; }
 
+        [MaxLength(500, ErrorMessage = ProductOptionDescriptionTooLongErrorMessage)]
         public string Description { get; set; }
     }
 }
