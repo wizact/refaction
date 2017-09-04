3aca2037b097953e8367cb1fa33962ee56739305
Amir Mohtasebi <amir.mohtasebi@gmail.com>
2017-09-04 19:20:06 +1200

    Add second controller and it's tests

---
+++ refactor-me.Tests/ProductOptionsControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using MSTestExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using refactor_me.Business.Services.Interfaces;
+using refactor_me.Controllers;
+using refactor_me.Models;
+using System;
+using System.Web.Http;
+
+namespace refactor_me.Tests
+{
+    [TestClass]
+    public class ProductOptionsControllerTests: BaseTest
+    {
+        [TestMethod]
+        public void when_returning_product_options_then_existence_of_product_should_get_validated_first()
+        {
+            // Arrange:
+            var productServiceMock = new Mock<IProductService>();
+            productServiceMock.Setup(m => m.GetProductById(It.IsAny<Guid>())).Returns(default(Product));
+
+            var productOptionServiceMock = new Mock<IProductOptionService>();
+            var productsController = new ProductOptionsController(productServiceMock.Object, productOptionServiceMock.Object);
+
+            // Act:
+            Action getProductOptions = () => productsController.GetOptions(Guid.NewGuid());
+
+            // Assert:
+            Assert.Throws<HttpResponseException>(getProductOptions);
+        }
+
+        [TestMethod]
+        public void when_returning_a_product_option_then_existence_of_product_should_get_validated_first()
+        {
+            // Arrange:
+            var productServiceMock = new Mock<IProductService>();
+            productServiceMock.Setup(m => m.GetProductById(It.IsAny<Guid>())).Returns(default(Product));
+
+            var productOptionServiceMock = new Mock<IProductOptionService>();
+            var productsController = new ProductOptionsController(productServiceMock.Object, productOptionServiceMock.Object);
+
+            // Act:
+            Action getProductOption = () => productsController.GetOption(Guid.NewGuid(), Guid.NewGuid());
+
+            // Assert:
+            Assert.Throws<HttpResponseException>(getProductOption);
+        }
+    }
+}
---
+++ refactor-me/Controllers/ProductOptionsController.cs
@@ -0,0 +1,100 @@
+﻿using refactor_me.Business.Services.Interfaces;
+using refactor_me.Models;
+using refactor_me.Validators;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace refactor_me.Controllers
+{
+    [RoutePrefix("products")]
+    public class ProductOptionsController : ApiController
+    {
+        private readonly IProductService _productService;
+        private readonly IProductOptionService _productOptionService;
+
+        public ProductOptionsController(IProductService productService, IProductOptionService productOptionService)
+        {
+            _productService = productService;
+            _productOptionService = productOptionService;
+        }
+
+        [Route("{productId}/options")]
+        [HttpGet]
+        public ProductOptions GetOptions(Guid productId)
+        {
+            ValidateProductExists(productId);
+
+            return _productOptionService.GetProductOptionsForProduct(productId);
+        }
+
+        [Route("{productId}/options/{id}")]
+        [HttpGet]
+        public ProductOption GetOption(Guid productId, Guid id)
+        {
+            ValidateProductExists(productId);
+
+            var productOption = _productOptionService.GetProductOption(id);
+
+            if (productOption == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return productOption;
+        }
+
+        [Route("{productId}/options")]
+        [HttpPost]
+        public HttpResponseMessage CreateOption(Guid productId, ProductOption productOption)
+        {
+            ValidateProductExists(productId);
+
+            new ProductOptionValidator().Validate(productOption);
+
+            productOption.Id = Guid.NewGuid();
+            productOption.ProductId = productId;
+            _productOptionService.CreateProductOptionForProduct(productOption);
+
+            var response = new HttpResponseMessage(HttpStatusCode.Created);
+            response.Headers.Location = new Uri($"/Products/{productId}/Options/{productOption.Id}", UriKind.Relative);
+            return response;
+        }
+
+        [Route("{productId}/options/{id}")]
+        [HttpPut]
+        public void UpdateOption(Guid productId, Guid id, ProductOption productOption)
+        {
+            ValidateProductExists(productId);
+
+            var existingProductOption = _productOptionService.GetProductOption(id);
+            new ProductOptionValidator().Validate(productOption);
+
+            if (existingProductOption == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            existingProductOption.Name = productOption.Name;
+            existingProductOption.Description = productOption.Description;
+
+            _productOptionService.UpdateProductOption(existingProductOption);
+        }
+
+        [Route("{productId}/options/{id}")]
+        [HttpDelete]
+        public void DeleteOption(Guid productId, Guid id)
+        {
+            ValidateProductExists(productId);
+            _productOptionService.DeleteProductOption(id);
+        }
+
+        private void ValidateProductExists(Guid productId)
+        {
+            var product = _productService.GetProductById(productId);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
